de4d94ad03e8d006bd31659208dd507113e4c1d3
alexpinch <alexpinch@hotmail.com>
2024-08-30 18:10:28 -0700

    added plot

---
+++ temp.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(ggsci)
+setwd("/Users/alexpinch/GitHub/public/birds_of_palestine") #data
+df <- read.csv("observations-472964.csv")
+plot1 <- df %>%
+  filter() %>%
+  group_by(common_name) %>%
+  filter(common_name != "") %>%
+  summarise(Count = n()) %>%
+  filter(Count >= 10) %>%
+  arrange(desc(Count)) %>%
+  mutate(common_name = factor(common_name, levels = common_name)) %>%
+  ggplot(aes(x = common_name, y = Count, fill = common_name)) + 
+  geom_bar(stat = "identity") +
+  guides(fill = "none") +
+  labs(x = "Bird Species", y = "Number of iNat observations") +
+  theme_classic() +
+  theme(axis.text.x=element_text(face="italic",angle=45, vjust=1, hjust=1), 
+        plot.margin=margin(t=10,r=10,b=10,l=45))
+plot1
+ggsave(filename = "num_obs.jpeg", plot = plot1, height = 5, width = 7, units = "in")
