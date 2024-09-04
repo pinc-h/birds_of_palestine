998dbb9e4e7badf4d952c66f1d0ca8db72b9b3a7
alexpinch <alexpinch@hotmail.com>
2024-09-04 13:39:40 -0700

    Update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Alex Pinch, lasted edited Aug 1st 2024
-# usage: `bash get_obs.sh observations-465502.csv`
+# usage: `bash make_html.sh observations-472964.csv`
 
 # check if the CSV file is provided
 if [ $# -ne 1 ]; then
@@ -21,34 +21,12 @@ echo "<!DOCTYPE html>
 <html>
 <head>
     <title>Birds of Palestine</title>
-    <style>
-        .gallery {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
-        .gallery img {
-            max-width: 100%;
-            height: auto;
-            margin: 10px;
-        }
-        .gallery a {
-            text-align: center;
-            flex: 1 0 100%;
-        }
-        @media (min-width: 768px) {
-            .gallery a {
-                flex: 1 0 45%;
-            }
-        }
-    </style>
 </head>
 <body>" > "$output_html"
 
 # adding text, opening div for the images below
 echo "<h1>Birds of Palestine</h1>" >> "$output_html"
-echo "<p>All iNaturalist observations of birds in Palestine in the last year. Last updated August 1st 2024.</p><br>" >> "$output_html"
-echo "<div class=\"gallery\">" >> "$output_html"
+echo "<p>All iNaturalist observations of birds in Palestine in the last year. Last updated Sept 4th 2024.</p><br>" >> "$output_html"
 
 # Reading inat data from CSV, skipping empty images and adding links the images
 awk -F ',' 'NR>1 && $14 != "" {print $13 "," $14}' "$csv_file" | while IFS=, read -r url image_url; do
@@ -56,7 +34,6 @@ awk -F ',' 'NR>1 && $14 != "" {print $13 "," $14}' "$csv_file" | while IFS=, rea
 done
 
 # closing HTML tags
-echo "</div>" >> "$output_html"
 echo "</body>" >> "$output_html"
 echo "</html>" >> "$output_html"
 
